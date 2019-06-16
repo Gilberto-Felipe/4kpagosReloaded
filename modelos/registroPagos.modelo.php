2a3e82ae93612794df7f5b1672a7197b257fcb7f
gil <gramirez25@ucol.mx>
2019-06-16 15:17:26 -0500

    cambiando ids en pagos y añadiendo modelo EER

--- modelos/registroPagos.modelo.php
+++ modelos/registroPagos.modelo.php
@@ -14,8 +14,8 @@ class ModeloRegistroPagos {
 
         $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla (id_usuario, id_deuda, cantidadPagada, fechaPago) VALUES(:id_usuario, :id_deuda, :cantidadPagada, NOW())");
 
-        $stmt -> bindParam(":id_usuario", $idDeuda, PDO::PARAM_STR);
         $stmt -> bindParam(":id_deuda", $idDeudor, PDO::PARAM_STR);
+        $stmt -> bindParam(":id_usuario", $idDeuda, PDO::PARAM_STR);
         $stmt -> bindParam(":cantidadPagada", $cantidadPagada, PDO::PARAM_STR);
 
         //$stmt-> debugDumpParams();
